df566df9df8a4217e0e1f9e27e5b3581ed362ec0
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-10-03 19:52:52 -0400

    creates models and routes

---
+++ app/routes.py
@@ -0,0 +1,57 @@
+from flask import jsonify, request
+from app import app, db
+from .models import Game, User
+
+# CRUD Operations for Game Model
+@app.route('/games', methods=['POST'])
+def create_game():
+    game = Game(title=request.json['title'])
+    db.session.add(game)
+    db.session.commit()
+    return jsonify(game.id), 201
+
+@app.route('/games', methods=['GET'])
+def get_games():
+    games = Game.query.all()
+    return jsonify([game.title for game in games])
+
+@app.route('/games/<int:id>', methods=['GET'])
+def get_game(id):
+    game = Game.query.get_or_404(id)
+    return jsonify(game.title)
+
+@app.route('/games/<int:id>', methods=['PUT'])
+def update_game(id):
+    game = Game.query.get_or_404(id)
+    game.title = request.json['title']
+    db.session.commit()
+    return jsonify(game.title)
+
+@app.route('/games/<int:id>', methods=['DELETE'])
+def delete_game(id):
+    game = Game.query.get_or_404(id)
+    db.session.delete(game)
+    db.session.commit()
+    return '', 204
+
+# User Registration & Login
+@app.route('/register', methods=['POST'])
+def register():
+    user = User(username=request.json['username'])
+    user.set_password(request.json['password'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.id), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User.query.filter_by(username=request.json['username']).first()
+    if user and user.check_password(request.json['password']):
+        # Here you can generate a token or create a session
+        return jsonify(success=True)
+    return jsonify(success=False), 401
+
+# Error Handling (simple example for 404)
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify(error=str(e)), 404
