624c1493a1168bfe9eab3a25701e03fb91bc3b92
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-10-03 22:36:26 -0400

    Updates app.js

--- game-trading-client/src/App.js
+++ game-trading-client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import GameList from './components/GameList';
 import UserProfile from './components/UserProfile';
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/games" component={GameList} />
         <Route path="/profile" component={UserProfile} />
         <Route path="/register" component={Register} />
+        <Route path="/login" element={<Login />} />
         
       </Switch>
     </Router>
