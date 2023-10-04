0b6816180adca56307e924a14b113f90d36a3380
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-10-03 20:14:04 -0400

    updates routes

--- game-trading-client/app/init.py
+++ game-trading-client/app/init.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  
