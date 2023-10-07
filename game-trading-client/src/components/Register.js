6d2019fed90137ff96c2de22de61dd2a84a10d07
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-10-07 19:23:22 -0400

    updates register

--- game-trading-client/src/components/Register.js
+++ game-trading-client/src/components/Register.js
@@ -1,44 +1,59 @@
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-const SignupSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(4, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  password: Yup.string()
-    .min(8, 'Too Short!')
-    .required('Required'),
+const RegistrationSchema = Yup.object().shape({
+    username: Yup.string()
+        .min(4, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    password: Yup.string()
+        .min(8, 'Too Short!')
+        .required('Required')
 });
 
 const Register = () => {
-  return (
-    <Formik
-      initialValues={{ username: '', password: '' }}
-      validationSchema={SignupSchema}
-      onSubmit={values => {
-        axios.post('/register', values)
-          .then(response => {
-            // Handle success
-          })
-          .catch(error => {
-            // Handle error
-          });
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <Field name="username" />
-          {errors.username && touched.username ? <div>{errors.username}</div> : null}
-          <Field name="password" type="password" />
-          {errors.password && touched.password ? <div>{errors.password}</div> : null}
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
-  );
-}
+    const [errorMessage, setErrorMessage] = useState(null);
+    const history = useHistory();
+
+    const handleRegistration = async (values) => {
+        try {
+            const response = await axios.post('/register', values);
+            if (response.status === 201) {
+                history.push('/login');
+            }
+        } catch (error) {
+           
+            setErrorMessage("Registration failed!");
+        }
+    };
+
+    return (
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            validationSchema={RegistrationSchema}
+            onSubmit={handleRegistration}
+        >
+            {() => (
+                <Form>
+                    <div>
+                        <label>Username:</label>
+                        <Field name="username" />
+                        <ErrorMessage name="username" component="div" />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <Field name="password" type="password" />
+                        <ErrorMessage name="password" component="div" />
+                    </div>
+                    <button type="submit">Register</button>
+                    {errorMessage && <div>{errorMessage}</div>}
+                </Form>
+            )}
+        </Formik>
+    );
+};
 
 export default Register;
