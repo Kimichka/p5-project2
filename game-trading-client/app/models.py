55c494f0436cacd416395a95ae8604e182f9d00f
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-10-08 16:59:56 -0400

    add files

--- game-trading-client/app/models.py
+++ game-trading-client/app/models.py
@@ -1,5 +1,9 @@
 from app import db, bcrypt
 from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
