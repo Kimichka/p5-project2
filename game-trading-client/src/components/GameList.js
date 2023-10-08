49ff6deef77023cb05d26017e87de64a97bd588f
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-10-08 16:23:54 -0400

    updates gamelist

--- game-trading-client/src/components/GameList.js
+++ game-trading-client/src/components/GameList.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import GameTable from './GameTable';
+import GameForm from './GameForm';
 
-const GameList = () => {
+function GameList() {
     const [games, setGames] = useState([]);
 
+    const fetchGames = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/games'); // Adjust the URL if your backend is running on a different address
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setGames(data);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error.message);
+        }
+    };
+
     useEffect(() => {
-        fetch('/games')
-            .then(response => response.json())
-            .then(data => setGames(data));
+        fetchGames();
     }, []);
 
     return (
         <div>
-            <h1>Games</h1>
-            <ul>
-                {games.map(game => <li key={game.id}>{game.title}</li>)}
-            </ul>
+            <h2>Game List</h2>
+            <GameForm onNewGame={fetchGames} />
+            <GameTable games={games} />
         </div>
     );
 }
